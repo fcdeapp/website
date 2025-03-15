e025ed4228a5e84669a28b6bdaccecc2233a388d
fcdeapp <tommydoh@fcde.app>
2025-03-15 16:24:03 +0900

    S

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import "./globals.css";
 const pretendard = localFont({
   src: [
     {
-      path: "../public/fonts/Pretendard-Regular.woff2",
+      path: "../../public/fonts/Pretendard-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/fonts/Pretendard-Bold.woff2",
+      path: "../../public/fonts/Pretendard-Bold.woff2",
       weight: "700",
       style: "normal",
     },
