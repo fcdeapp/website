4186b34ad87970c4dc70da1f70f3516df6d3fd5b
fcdeapp <tommydoh@fcde.app>
2025-03-22 00:18:44 +0900

    A

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,20 @@ export default function Header() {
         <Link href="/">
           <img src="/FacadeWebLogo.png" alt="Facade Logo" className="logo" />
         </Link>
-        <div className="nav-links flex gap-6">
+
+        <div className="nav-links flex gap-8">
           <Link href="/about">About</Link>
-          <Link href="/features">Features</Link>
+          <Link href="/posts">Posts</Link>
           <Link href="/terms">Terms</Link>
+        </div>
+
+        <div className="action-buttons flex gap-4">
+          <Link href="/login">
+            <a className="login-button">Login</a>
+          </Link>
+          <Link href="/signup">
+            <a className="signup-button">Sign Up</a>
+          </Link>
         </div>
       </nav>
     </header>
