31cd9f9620db0008853784d8c9377f45a9928fb4
fcdeapp <tommydoh@fcde.app>
2025-03-21 13:29:49 +0900

    A

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ export default function Header() {
     <header className="header">
       <nav className="header-nav container mx-auto flex items-center justify-between">
         <Link href="/">
-          <img src="/FacadeWebLogo.png" alt="Facade Logo" className="h-8 w-auto" />
+          <img src="/FacadeWebLogo.png" alt="Facade Logo" className="logo" />
         </Link>
         <div className="nav-links flex gap-6">
-          <Link href="/about"><a>About</a></Link>
-          <Link href="/features"><a>Features</a></Link>
-          <Link href="/terms"><a>Terms</a></Link>
+          <Link href="/about">About</Link>
+          <Link href="/features">Features</Link>
+          <Link href="/terms">Terms</Link>
         </div>
       </nav>
     </header>
