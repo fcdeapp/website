9e16ec673370f812876102e95f538e6be995489d
fcdeapp <fcdeapp@users.noreply.github.com>
2025-04-05 05:20:29 +0000

    deploy: 83c65a2a60ff40b190609d5f7cb4379d2c4ca243

---
+++ _next/static/chunks/app/startPage/page-0f868e95e8c29377.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[970],{2740:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>t});var n=r(5155),u=r(6745);function t(){return(0,n.jsx)(u.A,{})}},5785:(e,s,r)=>{Promise.resolve().then(r.bind(r,2740))}},e=>{var s=s=>e(e.s=s);e.O(0,[416,218,625,82,745,441,684,358],()=>s(5785)),_N_E=e.O()}]);
