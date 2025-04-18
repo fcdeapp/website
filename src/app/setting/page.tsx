43d706e214adc68063c68e4adbf152a925cd7f94
fcdeapp <tommydoh@fcde.app>
2025-04-18 23:58:11 +0900

    A

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 
