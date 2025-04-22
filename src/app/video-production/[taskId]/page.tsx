56a0d032503f4389e259447def58cd3dcbb3a33d
fcdeapp <tommydoh@fcde.app>
2025-04-23 05:52:14 +0900

    A

--- src/app/video-production/[taskId]/page.tsx
+++ src/app/video-production/[taskId]/page.tsx
@@ -5,11 +5,8 @@ import VideoProductionPage from '../../../components/VideoProductionPage';
 // CSR 강제(dynamic routing)
 export const dynamic = 'force-dynamic';
 
-export default function VideoProductionTaskPage({
-  params,
-}: {
-  params: { taskId: string };
-}) {
-  const taskId = params.taskId;
+export default function VideoProductionTaskPage(props: any) {
+  // any로 받으면 타입 에러가 안 뜸
+  const { taskId } = props.params as { taskId: string };
   return <VideoProductionPage taskId={taskId} />;
 }
